2650168c305c2e8c017b79207a3849d9e4b2da01
alexm323 <alejandromartinezjr42@gmail.com>
2021-07-25 12:18:36 -0700

    added in password hashing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import * as mongoose from 'mongoose'
-
+import bcrypt from 'bcryptjs'
 const UserSchema = new mongoose.Schema({
     username : {
         type: String,
@@ -24,7 +24,21 @@ const UserSchema = new mongoose.Schema({
         default:[]
     }
 })
+// going to hash our password using bcrypt and mongoose 
 
+UserSchema.pre("save", async function(next){
+    const user = this;
+    
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(user.password,salt);
+        user.password = hash
+
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
 
 // we can use a hook on the schema so that if we try to create a duplicate user we can get a different error message
 UserSchema.post('save',async(error,doc,next)=> {
